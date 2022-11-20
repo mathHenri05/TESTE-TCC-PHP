9978919aefcd974a93bc20fef499ec493c80730c
mathHenri05 <99218966+mathHenri05@users.noreply.github.com>
2022-11-19 22:17:41 -0300

    Add files via upload

---
+++ perfil.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="perfil.css">
+    <title>Aprensi</title>
+</head>
+<body>
+     <!-- Header -->
+     <header>
+        <!-- Nav -->
+        <div class="nav container">
+            <!-- Logo -->
+            <a href="index.html" class="logo">
+                <img src="img/aprensi_logo.png" alt="" id="aprensi-logo">
+            </a>
+            <!-- Search Box -->
+            <div class="search-box">
+                <input type="search" name="" id="search-input" placeholder="Pesquisar">
+                <i class='bx bx-search'></i>
+            </div>
+            <a href="perfil.html" class="user">
+                <i class='bx bx-user'></i>
+                <h1>MEU PERFIL</h1>
+            </a>
+        </div>
+    </header>
+
+    <section class="section-perfil-usuario">
+        <div class="perfil-usuario-header">
+            <div class="perfil-usuario-portada"></div>
+                <div class="perfil-usuario-avatar">
+                    <img src="assets/sombra laranja.jpeg" alt="img-avatar">
+                </div>
+            </div>
+        </div>
+        <div class="perfil-usuario-body">
+            <div class="perfil-usuario-bio">
+                <h3 class="titulo">Taina Ferreira</h3>
+                <p class="texto">Aluna</p>
+                <div class="info-seguidores">
+                    <p class="seguindo">Seguindo <br>3000</p> 
+                    <p class="seguidores">Seguidores <br>1000</p>
+                </div>
+            </div>
+            <div class="perfil-usuario-footer">
+                <div class="lista-dados">
+                    <li><i class='bx bx-book-bookmark'></i>Matéria</li>
+                    <li><i class='bx bxs-school' ></i>Instituição</li>
+                    <li><i class="icono icon-image"></i></li>
+                    <li><i class="icono icon-image"></i></li>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html> 
+
+
+   
+    
+
+    
---
+++ video.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aprensi</title>
+    <!-- Link To CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Link Swiper CSS -->
+    <link rel="stylesheet" href="swiper-bundle.min.css">
+    <!--Fav Icon-->
+    <link rel="shortcut icon" href="img/fav-icon.png" type="image/x-icon">
+    <!-- Box Icon -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <!-- Header -->
+    <header>
+        <!-- Nav -->
+        <div class="nav container">
+            <!-- Logo -->
+            <a href="index.html" class="logo">
+                <img src="img/aprensi_logo.png" alt="" id="aprensi-logo">
+            </a>
+            <!-- Searc Box -->
+            <div class="search-box">
+                <input type="search" name="" id="search-input" placeholder="Pesquisar">
+                <i class='bx bx-search'></i>
+            </div>
+            <a href="perfil.html" class="user">
+                <i class='bx bx-user'></i>
+                <h1>MEU PERFIL</h1>
+            </a>
+        </div>
+    </header>
+</body>
+</html>
